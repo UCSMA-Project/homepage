707a238ddbfbd28a2d8536ca5ead2b906bfecb0a
Qiwen Hua <hqw@huaqiwen.com>
2022-03-13 16:26:25 -0400

    add useState hook for user email input

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css'
 import header_iot from '../../assets/header_iot.png'
 
 const Header = () => {
+  /**
+   * Setup useState hook for email input state varaible
+   */
+  const [emailInput, setEmailInput] = useState('');
+
+  /**
+   * Handles user clicking "Join Us" button event
+   */
+  const handleJoinUs = () => {
+    console.log('User input email:', emailInput);
+  }
+
   return (
     <div className='ucsma__header section__padding' id='home'>
       <div className='ucsma__header-content'>
@@ -11,8 +23,13 @@ const Header = () => {
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, repellat! Harum quasi vitae molestias, eum quisquam blanditiis, odio perspiciatis reprehenderit rem sint consequuntur, tempora iusto! Officiis eos nulla quisquam perferendis?</p>
 
         <div className='ucsma__header-content__input'>
-          <input type='email' placeholder='Your email' />
-          <button type='button'>Join Us</button>
+          <input 
+            type='email' 
+            placeholder='Your Email' 
+            value={emailInput} 
+            onInput={e => setEmailInput(e.target.value)} 
+          />
+          <button type='button' onClick={handleJoinUs}>Join Us</button>
         </div>
       </div>
       
