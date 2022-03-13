4f348af098e4e7d97ef813f2dc1169771b039152
Qiwen Hua <hqw@huaqiwen.com>
2022-03-13 12:28:53 -0400

    add basic structure for containers and components

---
+++ src/containers/whats-ucsma/WhatsUCSMA.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whats-ucsma.css'
+
+const WhatsUCSMA = () => {
+  return (
+      <div>
+          WhatsUCSMA
+      </div>
+  );
+};
+
+export default WhatsUCSMA;
