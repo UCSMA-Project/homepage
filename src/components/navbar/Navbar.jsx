af27aefd81696cad11597385618814c68f0f2809
Qiwen Hua <hqw@huaqiwen.com>
2022-03-13 15:15:24 -0400

    implement navbar; add logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { DiGithubAlt, DiGithubFull } from 'react-icons/di'
 import './navbar.css'
+import ucsma_logo from '../../assets/ucsma_logo.png'
+
+const Menu = () => (
+  <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#wucsma'>What is UCSMA</a></p>
+    <p><a href='#features'>Features</a></p>
+    <p><a href='https://ucsma-project.github.io/docs/'>Documentations</a></p>
+  </>
+)
 
 const Navbar = () => {
   return (
-      <div>
-          Navbar
+    <div className='ucsma__navbar'>
+      <div className='ucsma__navbar-links'>
+        <div className='ucsma__navbar-links_logo'>
+          <img src={ucsma_logo} alt='UCSMA Logo'></img>
+        </div>
+
+        <div className='ucsma__navbar-links_container'>
+          <Menu />
+        </div>
       </div>
+
+      <div className='ucsma__navbar-github'>
+        <p><a href='https://github.com/UCSMA-Project/'>
+          <DiGithubAlt style={{fontSize: '40px'}}/>
+          <DiGithubFull style={{fontSize: '60px'}}/>
+        </a></p>
+      </div>
+
+    </div>
   );
 };
 
