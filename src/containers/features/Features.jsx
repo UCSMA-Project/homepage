8b043d45d18c37e1590e66e442592b4548cf03e1
Yufan Qian <lynn.qian@mail.utoronto.ca>
2022-03-13 20:43:40 -0400

    add feature page

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import './features.css'
+import { Feature } from '../../components';
+import './features.css';
 
 const Features = () => {
   return (
-      <div>
-          Features
+    <div className='ucsma__features section__margin' id='features'>
+
+      <div className='features-heading'>
+        <h1 className='gradient__text'>The future is now and you just need to realize it. Here are the features we have in UCSMA: </h1>
       </div>
+
+      <div className='features-container'>
+        <Feature title={"Feature 1"} text={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae assumenda reiciendis sit ad hic quisquam, ducimus officiis."} />
+      
+        <Feature title={"Feature 2"} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam similique et totam, ipsam magni harum temporibus animi odio cupiditate alias accusamus esse officia voluptas aperiam a dolores facilis sit eveniet."} />
+      
+        <Feature title={"Feature 3"} text={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, repellat!"} />
+
+        <Feature title={"Feature 4"} text={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, repellat!"} />
+      </div>
+  </div>
   );
 };
 
